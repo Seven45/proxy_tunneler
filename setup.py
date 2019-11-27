cd0ffe4b6fb86c4c1802755295c1eac522d24099
s.dubrovin <s.dubrovin@tinkoff.ru>
2019-11-27 15:44:09 +0500

    fix dependencies

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
-from setuptools import setup, find_packages
 from os.path import join, dirname
+
+from setuptools import setup, find_packages
+
+long_description = open(join(dirname(__file__), 'README.rst'), 'r').read()
+
+install_requires = open((join(dirname(__file__), 'requirements.txt')), 'r').readlines()
+install_requires = list(map(lambda line: line.replace('\n', ''), install_requires))
 
 
 setup(name='ProxyTunneller',
-      version='0.0.1',
+      version='0.2.0',
       url='https://github.com/Seven45/ProxyTunneller',
       author='Dubrovin Semyon',
       author_email='seven45@mail.ru',
       description='Library for create proxy tunnels',
-      long_description=open(join(dirname(__file__), 'README.rst'), 'r').read(),
+      long_description=long_description,
       packages=find_packages(),
+      install_requires=install_requires,
       python_requires='>=3.7')
