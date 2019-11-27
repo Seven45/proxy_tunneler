e840a8be9430b672250aecb9d0272a98da00fa73
s.dubrovin <s.dubrovin@tinkoff.ru>
2019-11-27 16:00:45 +0500

    fix dependencies

--- setup.py
+++ setup.py
@@ -1,22 +1,17 @@
 from os.path import join, dirname
 
-from pip._internal.network.session import PipSession
-from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
 long_description = open(join(dirname(__file__), 'README.rst'), 'r').read()
 
-requirements = parse_requirements('requirements.txt', session=PipSession())
-install_requires = [str(req.req) for req in requirements]
-
 
 setup(name='ProxyTunneller',
-      version='0.2.1',
+      version='0.2.2',
       url='https://github.com/Seven45/ProxyTunneller',
       author='Dubrovin Semyon',
       author_email='seven45@mail.ru',
       description='Library for create proxy tunnels',
       long_description=long_description,
       packages=find_packages(),
-      install_requires=install_requires,
+      install_requires=['pproxy', 'aiohttp', 'aiosocksy'],
       python_requires='>=3.7')
